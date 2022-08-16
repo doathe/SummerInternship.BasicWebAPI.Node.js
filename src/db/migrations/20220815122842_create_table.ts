6e216df7118f2b864fb2ee164bb25fbf28b1a551
doathe <nisa.tur@gmail.com>
2022-08-16 13:25:50 +0300

    Updated at 16.08.2022 / 13.25

--- src/db/migrations/20220815122842_create_table.ts
+++ src/db/migrations/20220815122842_create_table.ts
@@ -3,12 +3,12 @@ import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
 
-    return knex.schema.createTable('user', function (table) {
-        table.increments('id');
-        table.string('name', 55).notNullable();
-        table.string('surname', 55).notNullable();
-        table.string('email', 150).notNullable().unique();
-        table.integer('age', 150).notNullable();
+    return knex.schema.createTable("user", (table) => {
+        table.increments("id").notNullable().primary();
+        table.string("name", 55).notNullable();
+        table.string("surname", 55).notNullable();
+        table.string("email", 150).notNullable().unique();
+        table.integer("age", 150).notNullable();
         table.timestamps(true, true);
     })
 }
